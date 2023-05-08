34ec39aa333008b51e3eb23c7b66c30872a5aa61
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2023-05-08 10:34:50 +0600

    Update hellovitalik.js

--- 01_HelloVitalik/hellovitalik.js
+++ 01_HelloVitalik/hellovitalik.js
@@ -1,16 +1,16 @@
-// 导入ethers包
+// import ethers package
 import { ethers } from "ethers";
-// playcode免费版不能安装ethers，用这条命令，需要从网络上import包（把上面这行注释掉）
+// playcode free version cannot install ethers, use this command, need to import package from the network (comment out the line above)
 // import { ethers } from "https://cdnjs.cloudflare.com/ajax/libs/ethers/6.2.3/ethers.js";
 
-// 利用ethers默认的Provider连接以太坊网络
+// use ethers default Provider to connect to Ethereum network
 // const provider = new ethers.getDefaultProvider();
 const ALCHEMY_MAINNET_URL = 'https://eth-mainnet.g.alchemy.com/v2/oKmOQKbneVkxgHZfibs-iFhIlIAl6HDN';
 const provider = new ethers.JsonRpcProvider(ALCHEMY_MAINNET_URL)
 
 const main = async () => {
-    // 查询vitalik的ETH余额
+    // query vitalik’s ETH balance
     const balance = await provider.getBalance(`vitalik.eth`);
-    // 将余额输出在console
+    //output the balance in the console
     console.log(`ETH Balance of vitalik: ${ethers.formatEther(balance)} ETH`);}
 main()
