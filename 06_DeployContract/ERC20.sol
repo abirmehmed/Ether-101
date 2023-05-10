3740376049affc9e4fb57f7c80620a1fb0377ac3
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2023-05-10 21:56:57 +0600

    Update ERC20.sol

--- 06_DeployContract/ERC20.sol
+++ 06_DeployContract/ERC20.sol
@@ -1,1 +1,54 @@
 
+// SPDX-License-Identifier: MIT
+// WTF Solidity by 0xAA
+
+pragma solidity ^0.8.4;
+
+import "./IERC20.sol";
+
+contract ERC20 is IERC20 {
+
+    mapping(address => uint256) public override balanceOf;
+
+    mapping(address => mapping(address => uint256)) public override allowance;
+
+    uint256 public override totalSupply;   // Total token supply
+
+    string public name;   // Name
+    string public symbol;  // Symbol
+    
+    uint8 public decimals = 18; // Decimal places
+
+    // @dev Implement the contract name and symbol at the time of contract deployment
+    constructor(string memory name_, string memory symbol_){
+        name = name_;
+        symbol = symbol_;
+    }
+
+    // @dev Implement `transfer` function, token transfer logic
+    function transfer(address recipient, uint amount) external override returns (bool) {
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    // @dev Implement `approve` function, token authorization logic
+    function approve(address spender, uint amount) external override returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    // @dev Implement `transferFrom` function, token authorized transfer logic
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external override returns (bool) {
+        allowance[sender][msg.sender] -= amount;
+        balanceOf[sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
