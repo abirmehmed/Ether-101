714a26ed8d162e19859518aa58a048e6cc7bc358
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2023-05-10 22:06:27 +0600

    Update IERC20.sol

--- 06_DeployContract/IERC20.sol
+++ 06_DeployContract/IERC20.sol
@@ -1,1 +1,67 @@
+// SPDX-License-Identifier: MIT
+// WTF Solidity by 0xAA
 
+pragma solidity ^0.8.4;
+
+/**
+ * @dev ERC20 interface contract.
+ */
+interface IERC20 {
+    /**
+     * @dev Emits when `value` units of currency are transferred from account (`from`) to another account (`to`).
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emits when `value` units of currency are authorized by account (`owner`) to another account (`spender`).
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /**
+     * @dev Returns the total token supply.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the number of tokens held by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Transfers `amount` units of token from the caller's account to another account `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the allowance of `spender` over `owner`'s tokens, defaulting to 0.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Authorizes `spender` to spend `amount` units of token on behalf of the caller's account.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Transfers `amount` units of token from `from` account to `to` account via authorization mechanism. The transferred part will be deducted from the caller's `allowance`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+}
