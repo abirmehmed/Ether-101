1aaea4f326058026d944a0c19f362fe0711af2b8
Islam Md Abirul <124920759+abirmehmed@users.noreply.github.com>
2023-05-22 00:17:28 +0600

    Update EncodeCalldata.js

--- 13_EncodeCalldata/EncodeCalldata.js
+++ 13_EncodeCalldata/EncodeCalldata.js
@@ -24,3 +24,63 @@ const addressWETH = '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6'
 // Declare WETH contract
 const contractWETH = new ethers.Contract(addressWETH, abiWETH, wallet)
 
+// Declare WETH contract
+const contractWETH = new ethers.Contract(addressWETH, abiWETH, wallet)
+
+const main = async () => {
+
+    const address = await wallet.getAddress()
+    // 1. Read the on-chain information of the WETH contract (WETH abi)
+    console.log("\n1. Read WETH balance")
+    // Encode calldata
+    const param1 = contractWETH.interface.encodeFunctionData(
+        "balanceOf",
+        [address]
+      );
+    console.log(`Encoding result: ${param1}`)
+    // Create transaction
+    const tx1 = {
+        to: addressWETH,
+        data: param1
+    }
+    // Send transaction, read-only operations (view/pure) can use provider.call(tx)
+    const balanceWETH = await provider.call(tx1)
+    console.log(`WETH balance before deposit: ${ethers.formatEther(balanceWETH)}\n`)
+
+    //Read ETH balance in wallet
+    const balanceETH = await provider.getBalance(wallet)
+    // If wallet has enough ETH
+    if(ethers.formatEther(balanceETH) > 0.0015){
+
+        // 2. Call desposit() function, convert 0.001 ETH to WETH
+        console.log("\n2. Call desposit() function, deposit 0.001 ETH")
+        // Encode calldata
+        const param2 = contractWETH.interface.encodeFunctionData(
+            "deposit"          
+            );
+        console.log(`Encoding result: ${param2}`)
+        // Create transaction
+
+       const tx2 = {
+            to: addressWETH,
+            data: param2,
+            value: ethers.parseEther("0.001")}
+        // Send transaction, write operations need wallet.sendTransaction(tx)
+        const receipt1 = await wallet.sendTransaction(tx2)
+        // Wait for transaction to be mined
+        await receipt1.wait()
+        console.log(`Transaction details:`)
+        console.log(receipt1)
+        const balanceWETH_deposit = await contractWETH.balanceOf(address)
+        console.log(`WETH balance after deposit: ${ethers.formatEther(balanceWETH_deposit)}\n`)
+
+    }else{
+        // If ETH is not enough
+        console.log("ETH is not enough, go to the faucet to get some Goerli ETH")
+        console.log("1. chainlink faucet: https://faucets.chain.link/goerli")
+        console.log("2. paradigm faucet: https://faucet.paradigm.xyz/")
+    }
+}
+
+main()
+
